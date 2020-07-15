976e8af76b1587c97d564342bf20143e683cca2a
gonzalorqn <gonzalo.rqn@gmail.com>
2020-07-15 01:24:02 -0300

    solucion error

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -18,6 +18,7 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    localStorage.setItem('token', null);
     try {
       const token = localStorage.getItem('token');
       const helper = new JwtHelperService();
